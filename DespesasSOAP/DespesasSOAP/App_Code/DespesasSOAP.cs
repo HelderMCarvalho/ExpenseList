b5616be38831f61d38262d80db41910296308b3a
HelderMCarvalho <a15310@alunos.ipca.pt>
2020-12-15 01:19:16 +0000

    ADD SOAP and REST Servives (base)

---
+++ DespesasSOAP/DespesasSOAP/App_Code/DespesasSOAP.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "DespesasSOAP" in code, svc and config file together.
+public class DespesasSOAP : IDespesasSOAP
+{
+    public string GetData(int value) {
+        return string.Format("You entered: {0}", value);
+    }
+
+    public CompositeType GetDataUsingDataContract(CompositeType composite) {
+        if(composite == null)
+        {
+            throw new ArgumentNullException("composite");
+        }
+        if(composite.BoolValue)
+        {
+            composite.StringValue += "Suffix";
+        }
+        return composite;
+    }
+}
